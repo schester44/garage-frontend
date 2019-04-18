ade0a91e21404625126afa26e303bda0fd1b8216
Steven Chester <steven.chester@industryweapon.com>
2019-04-18 18:53:30 -0400

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# My Garage
+
+Simple frontend that shows whether my garage door is open or not.
+
+The backend to this will text me if its been open for longer than a minute.
+
+
+## Why? 
+Why not? Sometimes i'm forgetful and forget to close the garage door.
---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React from "react"
+import styled from "styled-components"
+import axios from "axios"
+import { format, addSeconds, isBefore } from "date-fns"
+
+const Container = styled("div")`
+	width: 100%;
+	height: 100%;
+
+	background: ${({ isOpen }) => (isOpen ? "red" : "green")};
+	color: white;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+
+	h1 {
+		font-size: 20vw;
+	}
+
+	h2 {
+		font-size: 8vw;
+	}
+`
+
+const App = () => {
+	const [{ status, lastOpen, lastCheck, limit }, setState] = React.useState({})
+
+	React.useEffect(() => {
+		const checkStatus = () => {
+			// Door has been open for max limit
+			if (isBefore(lastOpen, addSeconds(new Date(), limit))) {
+				var audio = new Audio("alarm.mp3")
+				audio.play()
+			}
+
+			axios.get(process.env.API_ENDPOINT).then(({ data }) => {
+				setState(data)
+
+				// check if the door is still open in 2 seconds
+				setTimeout(() => checkStatus(), 2000)
+			})
+		}
+
+		checkStatus()
+	}, [])
+
+	const isOpen = status === "open"
+
+	if (!status) return null
+
+	return (
+		<Container isOpen={isOpen}>
+			<h2>Garage Door</h2>
+			<h1>{isOpen ? "Open" : "Closed"}</h1>
+
+			<h3>Last Checked: {format(lastCheck, "MMM Do, YYYY h:mm A")}</h3>
+			<h3>Last Opened: {format(lastOpen, "MMM Do, YYYY h:mm A")}</h3>
+		</Container>
+	)
+}
+
+export default App
